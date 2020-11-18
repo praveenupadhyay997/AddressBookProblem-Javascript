377faa99ad1a20c809c046cd9a6a8e936ca0ef69
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-11-19 01:08:45 +0530

    [Praveen] Add: Implemented the functionality that only the valid contact is added and for invalid contact throws error.

--- AddressBookProblem.js
+++ AddressBookProblem.js
@@ -1,15 +1,6 @@
 /// Defining the address book class with given properties
 class AddressBook
 {
-    /// Data attributes of the address book
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phoneNumber;
-    emailId;
     /// Parameterised constructor initialising the instance with values passed
     constructor(...params)
     {
@@ -22,6 +13,87 @@ class AddressBook
         this.phoneNumber = params[6];
         this.emailId = params[7];
     }
+    /// Data attributes of the address book
+    /// Getter and Setter for the first name
+    get firstName() {return this._firstName;}
+    set firstName(firstName)
+    {
+        let NAME_REGEX= RegExp('^[A-Z]{1}[a-z]{2,}');
+        if (NAME_REGEX.test(firstName))
+        this._firstName=firstName;
+        else 
+        throw 'Invalid first name';
+    }
+    /// Getter and Setter for the last name
+    get lastName() {return this._lastName;}
+    set lastName(lastName)
+    {
+        let NAME_REGEX= RegExp('^[A-Z]{1}[a-z]{2,}');
+        if (NAME_REGEX.test(lastName))
+        this._lastName=lastName;
+        else 
+        throw 'Invalid last name';
+    }
+    /// Getter and Setter for the address
+    get address(){return this._address;}
+    set address(address)
+    {
+        let ADDRESS_REGEX = RegExp('^[A-Z]{1}[0-9-A-Za-z]{3,}$');
+        if(ADDRESS_REGEX.test(address))
+        this._address = address;
+        else 
+        throw 'Invalid Address';
+    }
+    /// Getter and Setter for the city
+    get city(){ return this._city;}
+    set city(city)
+    {
+        let CITY_REGEX = RegExp('^[A-Z]{1}[a-z]{3,}');
+        if(CITY_REGEX.test(city))
+        this._city=city;
+        else 
+        throw 'Invalid city entry as per regular expression';
+    }
+    /// Getter and Setter for the state
+    get state(){return this._state;}
+    set state(state)
+    {
+        let STATE_REGEX = RegExp('^[A-Z]{1}[a-z]{3,}');
+        if(STATE_REGEX.test(state))
+        this._state=state;
+        else 
+        throw 'Invalid state entry as per regular expression';
+    }
+    /// Getter and Setter for the zip
+    get zip(){return this._zip;}
+    set zip(zip)
+    {
+        let ZIP_REGEX = RegExp('^[0-9]{3}[ ]?[0-9]{3}$');
+        if(ZIP_REGEX.test(zip))
+        this._zip=zip;
+        else 
+        throw 'Invalid ZIP-CODE';
+    }
+    /// Getter and Setter for the phone number
+    get phoneNumber(){return this._phoneNumber;}
+    set phoneNumber(phoneNumber)
+    {
+        let phoneRegex = RegExp('^[0-9]{10}');
+        if(phoneRegex.test(phoneNumber))
+        this._phoneNumber=phoneNumber;
+        else 
+        throw 'Invalid Phone-Number';
+    }
+    /// Getter and Setter for the emailId
+    get emailId(){return this._emailId;}
+    set emailId(emailId){
+        let emailRegex = RegExp('^[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$');
+        if(emailRegex.test(emailId))
+        this._emailId=emailId;
+        else 
+        throw 'Invalid Email-Id';
+    }
+    /// Getter and Setter for the phone number
     /// Display the address book contact details
     DisplayContactDetails()
     {
@@ -30,10 +102,10 @@ class AddressBook
     }
 }
 /// Defining the address book instance with random details
-let addressBookList = [{firstName: "Raj", lastName: "Singh", address: "Sec-1", city: "Delhi", state: "Delhi", zip: 112012, phoneNumber: 9724512454, emailId: "raj@gmail.com"},
-                       {firstName: "Shikha", lastName: "Singh", address: "Sec-2", city: "Gorakhpur", state: "UP", zip: 274306, phoneNumber: 8645512454, emailId: "shikha@gmail.com"},
+let addressBookList = [{firstName: "Raj", lastName: "Singh", address: "Sec-1", city: "Delhi", state: "Delhi", zip: 112012, phoneNumber: 9724512445, emailId: "raj@gmail.com"},
+                       {firstName: "Shikha", lastName: "Singh", address: "Sec-2", city: "Gorakhpur", state: "UttarPradesh", zip: 274306, phoneNumber: 8645512454, emailId: "shikha@gmail.com"},
                        {firstName: "Vijay", lastName: "Goyal", address: "Sec-1", city: "Delhi", state: "Delhi", zip: 110012, phoneNumber: 8795512454, emailId: "vijay@gmail.com"},
-                       {firstName: "Alka", lastName: "Saini", address: "Sec-3", city: "Kanpur", state: "UP", zip: 341012, phoneNumber: 9724512454, emailId: "alka@gmail.com"},
+                       {firstName: "Alka", lastName: "Saini", address: "Sec-3", city: "Kanpur", state: "UttarPradesh", zip: 341012, phoneNumber: 9724512454, emailId: "alka@gmail.com"},
                        {firstName: "Shubham", lastName: "Sharma", address: "Sec-4", city: "Hisar", state: "Haryana", zip: 146112, phoneNumber: 7845412454, emailId: "shubham@gmail.com"},
                        {firstName: "Shardul", lastName: "Singh", address: "Sec-6", city: "Kurukshetra", state: "Haryana", zip: 136119, phoneNumber: 7206512454, emailId: "shardul@gmail.com"}];
 /// Storing this array objects in array of objects
